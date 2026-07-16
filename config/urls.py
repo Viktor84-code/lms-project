e9a339218df8234b22128e54bb95912f5347a244
Viktor <viktor@example.com>
2026-07-16 12:06:33 +0300

    feat: add CRUD for Course and Lesson, custom User model

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('lms.urls')),
+    path('', lambda request: redirect('api/', permanent=False)),  # редирект с корня на API
 ]
