937c2f084a23e728d9f830abc3796bc7c2108a8e
Viktor <viktor@example.com>
2026-07-16 11:28:05 +0300

    feat: add URLs for lms and users

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListCreateView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
