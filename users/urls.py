a00c871fe70dbeb66253ba1ab54de45d5573f164
Viktor <viktor@example.com>
2026-07-17 12:27:49 +0300

    feat: add payment list with filtering and ordering

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserProfileUpdateView
+from .views import UserProfileUpdateView, PaymentListView
 
 urlpatterns = [
     path("profile/<int:pk>/", UserProfileUpdateView.as_view(), name="profile-update"),
+    path('payments/', PaymentListView.as_view(), name='payment-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
 
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Payment
+from .serializers import UserSerializer, PaymentSerializer
 
 
 class UserProfileUpdateView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentListView(ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['course', 'lesson', 'payment_method']
+    ordering_fields = ['payment_date']
