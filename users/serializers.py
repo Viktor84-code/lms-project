8146ba2884e7efff3c32fe268a0f09a17613e6fc
Viktor <viktor@example.com>
2026-07-17 12:32:05 +0300

    feat: add payment history to user profile

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 
-from .models import Payment, User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "email", "phone", "city", "avatar"]
+from .models import User, Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar', 'payments']
