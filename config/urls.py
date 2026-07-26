af36b4890e3d68c4ca149c98507e57f31a4c3db7
Viktor <viktor@example.com>
2026-07-26 13:46:11 +0300

    feat: add JWT authentication with simplejwt

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import include, path
+from django.views.generic import RedirectView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("lms.urls")),
-    path("", lambda request: redirect("api/", permanent=False)),
+    path("", RedirectView.as_view(url="api/", permanent=False)),
     path("users/", include("users.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
