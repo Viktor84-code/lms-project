e8fda92bed0b015a807240daf81f4e4fcbd0717b
Viktor <viktor@example.com>
2026-07-29 13:51:00 +0300

    feat: add youtube validator and subscription model with endpoint

--- lms/views.py
+++ lms/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404
+from h11 import Response
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
 from lms.serializers import CourseSerializer, LessonSerializer
 from users.permissions import IsModer, IsOwner
 
@@ -58,3 +62,21 @@ class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
             # Просматривать могут модераторы ИЛИ владельцы
             self.permission_classes = [IsAuthenticated, IsModer | IsOwner]
         return super().get_permissions()
+
+
+class SubscriptionView(APIView):
+    def post(self, request):
+        user = request.user
+        course_id = request.data.get('course_id')
+        course = get_object_or_404(Course, id=course_id)
+
+        subscription = Subscription.objects.filter(user=user, course=course)
+
+        if subscription.exists():
+            subscription.delete()
+            message = 'Подписка удалена'
+        else:
+            Subscription.objects.create(user=user, course=course)
+            message = 'Подписка добавлена'
+
+        return Response({'message': message})
