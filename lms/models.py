4a7ca380f885b176e93147c85cf36a492d6eb45c
Viktor <viktor@example.com>
2026-08-03 16:07:13 +0300

    feat: add Stripe payment status, tests, and coverage 85%

--- lms/models.py
+++ lms/models.py
@@ -6,6 +6,7 @@ class Course(models.Model):
     title = models.CharField(max_length=200)
     preview = models.ImageField(upload_to="courses/", blank=True, null=True)
     description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=1000)
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
